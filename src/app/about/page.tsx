275d90a120b8b4b490e8a908a60c8f84ca168308
Anamay-Sarvate_bcgprod <anamay.sarvate@bcgdv.com>
2023-06-20 17:01:32 +0200

    feat: add course and repo features

---
+++ src/app/about/page.tsx
@@ -0,0 +1,40 @@
+export const metadata = {
+  title: "About title",
+};
+
+export const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi a
+        obcaecati aperiam in quibusdam nam placeat vero dolorum aliquid ad, sunt
+        mollitia est laborum dignissimos numquam tempore quo sapiente minima
+        corrupti voluptatibus voluptatum nesciunt eum? Culpa inventore quo
+        quisquam, placeat libero vero fugiat quod dolorum iure, totam dolores
+        accusamus nemo recusandae natus deleniti excepturi ullam nostrum nam
+        possimus dolor voluptatum! Rem a soluta labore dolorum, nisi repellat
+        nostrum veniam illum eius quidem quibusdam repudiandae illo adipisci
+        maxime iure dolores dignissimos quam quod vitae, error amet vero. Nobis
+        minima, ad eaque ratione quibusdam excepturi harum velit adipisci
+        veritatis repellendus iusto nesciunt ducimus incidunt. Consequuntur
+        quaerat ut earum mollitia placeat distinctio itaque nisi! Aliquid maxime
+        maiores saepe dolorem suscipit fugiat, architecto molestias doloribus in
+        atque ea adipisci nam, quo unde cumque cupiditate beatae natus tenetur
+        ad quidem, eum eius eveniet dolore! Perspiciatis ullam, sit vel earum
+        facere, animi nisi unde, iste ex consectetur quae? Aut delectus natus,
+        atque temporibus deserunt deleniti exercitationem tempore illum et
+        voluptates aspernatur at doloribus animi! Autem eaque, doloribus
+        temporibus voluptatem, adipisci voluptatum, illo voluptatibus reiciendis
+        cupiditate mollitia laborum a quia iure maxime eligendi labore animi
+        consectetur tenetur. Natus corrupti ipsa alias perferendis ab, sed sequi
+        consequuntur! Ex nulla consequatur eaque enim possimus laborum, ab
+        beatae minus illo accusamus esse. Harum totam error aut repudiandae
+        necessitatibus temporibus. Qui consequatur quas ipsum veritatis
+        perferendis.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import LoadingPage from "./loading";
+import Courses from "./components/Courses";
+import CourseSearch from "./components/CourseSearch";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
-};
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <>
+      <h1>Welcome To Traversy Meida</h1>
+      <CourseSearch getSearchResults={(results: any) => setCourses(results)} />
+      <Courses courses={courses} />
+    </>
+  );
+};
 export default HomePage;
